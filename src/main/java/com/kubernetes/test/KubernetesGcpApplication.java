2982c8d1a53fefb4699526e04e5c31d2bf36a1fd
abdelaziz <abdelaziz.elhassouni97@gmail.com>
2019-12-25 22:00:41 +0100

    commit

---
+++ src/main/java/com/kubernetes/test/KubernetesGcpApplication.java
@@ -0,0 +1,13 @@
+package com.kubernetes.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesGcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesGcpApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kubernetes/test/TestController.java
@@ -0,0 +1,19 @@
+package com.kubernetes.test;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+	
+	@GetMapping("message")
+	public String getMessage(){
+		return "Hello !!! I'm Spring Boot APP ...";
+	}
+}
